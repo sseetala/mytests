0dfd6fecdacf82e0eddf534a8f210887f6219cf6
sseetala <39924305+sseetala@users.noreply.github.com>
2019-05-05 18:24:30 -0700

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -68,6 +68,38 @@ class test_record(BaseTest):
         assert 1 == 1
 
 
+#########################
+import boto3
+session = boto3.session.Session(region_name='us-east-1')
+dynamodb = session.resource('dynamodb')
+table = dynamodb.Table('mytesttable')
+
+table_exists = False
+
+try:
+    table.creation_date_time
+    table_exists = True
+except:
+    table_exists = False
+
+if table_exists:
+    print("Table name already exists, please try with a different name")
+else:
+    new_table = dynamodb.create_table(
+        AttributeDefinitions=[
+            {
+                'AttributeName' : 'partition_key',
+                'AttributeType' : 'S',
+            },
+        ],
+        KeySchema=[
+            {
+                'AttributeName' : 'partition_key',
+                'KeyType' : 'HASH',
+            },
+        ],
+        TableName='mytable',
+    )
 
 
 
